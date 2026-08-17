9f246e7638070e60cfa2a54c67b87acd73c3d2e4
Steven Enamakel <enamakel@tinyhumans.ai>
2026-08-18 01:46:11 +0300

    chore(allowlist): add newly public repositories to the allowlist
    The allowlist of public repositories that are safe to send diffs from has been updated to include several new repositories that have been made public since the last update. This ensures that auto-commit continues to work correctly for all public repositories in the tinyhumansai and senamakel organizations.
    
    Auto-committed-on: macbook

--- tests/auto-commit.sh
+++ tests/auto-commit.sh
@@ -905,9 +905,9 @@ test_commits_only_in_allowlisted_repositories() {
   local slug n=1
   for slug in tinyhumansai/openhuman tinyhumansai/opencompany tinyhumansai/medulla tinyhumansai/rust-template \
               tinyhumansai/tinyagents tinyhumansai/tinybus tinyhumansai/tinychannels tinyhumansai/tinycortex \
-              tinyhumansai/tinydocs tinyhumansai/tinyflows tinyhumansai/tinymemory tinyhumansai/tinyruntime tinyhumansai/tinysweeper tinyhumansai/tinywallet \
+              tinyhumansai/tinybox tinyhumansai/tinydocs tinyhumansai/tinyflows tinyhumansai/tinyhosts tinyhumansai/tinymemory tinyhumansai/tinyruntime tinyhumansai/tinysweeper tinyhumansai/tinyvoice tinyhumansai/tinywallet \
               senamakel/openhuman senamakel/opencompany senamakel/medulla senamakel/riemann senamakel/rust-template \
-              senamakel/tinyagents senamakel/tinybus senamakel/tinychannels senamakel/tinycortex \
+              senamakel/llm-ladder-router senamakel/tinyagents senamakel/tinybus senamakel/tinychannels senamakel/tinycortex \
               senamakel/tinydocs senamakel/tinyflows senamakel/tinymemory senamakel/tinyruntime senamakel/tinysweeper senamakel/tinywallet; do
     git -C "$repo" remote add origin "git@github.com:$slug.git"
     AUTO_COMMIT_EVERY=1 fire "$repo" "$state" "$stub" >/dev/null
